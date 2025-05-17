ee1125704e409975933b28d634aeb53c789f5b06
KaranjitKahlon <kkahlon808@gmail.com>
2025-05-16 18:46:43 -0700

    added training model functionality and the ability to save and load the model state

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("test")
+import os
+import keras
+import cv2
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist = tf.keras.datasets.mnist # importing mnist data for handwritten number references
+(x_train, y_train), (x_test, y_test) = mnist.load_data() # train data
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(128,activation='relu'))
+model.add(tf.keras.layers.Dense(128,activation='relu'))
+model.add(tf.keras.layers.Dense(10,activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+# save state of model
+model.save('handwritten.model')
+
+# load model without having to train all over again
+model = tf.keras.models.load_model('handwritten.model')
